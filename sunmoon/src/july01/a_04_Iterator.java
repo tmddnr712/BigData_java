bd8a776737701383b7d32db7db19c9dcfc4b8a4c
tmddnr712 <nsu712@naver.com>
2021-07-02 11:09:33 +0900

    210702 HashMap

---
+++ sunmoon/src/july01/a_04_Iterator.java
@@ -0,0 +1,15 @@
+package july01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class a_04_Iterator {
+
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<>();
+		list.add("A");
+		list.add("B");
+		list.add("C");
+	}
+
+}
